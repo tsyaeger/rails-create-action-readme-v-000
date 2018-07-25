6989d1127e000b3806f1161f8e55096ad075474a
taryn yaeger <tsyaeger@gmail.com>
2018-07-25 01:27:30 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,14 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
+	
+	def create
+	  post = Post.new
+    post.title = params[:title]
+    post.description = params[:description]
+    post.save
+    redirect to post_path(@path)
+  end
+	
+	
 end
